4db4fbe04c75a33f184177de624c9c26fa8d5060
Abbas Ahmad <bbsahmad@gmail.com>
2025-12-18 12:48:05 +0530

    small fix

--- src/main/java/store/chat_storage/dto/UpdateChatSessionRequest.java
+++ src/main/java/store/chat_storage/dto/UpdateChatSessionRequest.java
@@ -4,10 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateChatSessionRequest {
     @NotBlank(message = "Session name is required")
     private String name;
